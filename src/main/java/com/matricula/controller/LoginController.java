11a5010becad22d3391eb5b7d53540dc39390419
Richard Joaquin <richardjoaquin99@gmail.com>
2020-06-25 22:10:43 -0500

    u6

--- src/main/java/com/matricula/controller/LoginController.java
+++ src/main/java/com/matricula/controller/LoginController.java
@@ -31,6 +31,6 @@ public class LoginController {
 			model.addAttribute("success", "Ha cerrado sesión con éxito!");
 		}
 
-		return "/login";
+		return "login";
 	}
 }
--- src/main/java/com/matricula/securityconfig/SpringSecurityConfig.java
+++ src/main/java/com/matricula/securityconfig/SpringSecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-//https://www.baeldung.com/spring-security-method-security
 @EnableGlobalMethodSecurity(securedEnabled=true)
 @EnableWebSecurity
 @Configuration
